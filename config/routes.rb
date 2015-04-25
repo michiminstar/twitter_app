90aabacb128ffed37ef3e8d3b8d4903d34392681
Michi Kanda <michi.kanda.0513@gmail.com>
2015-04-25 12:48:24 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 TwitterApp::Application.routes.draw do
   root 'static_pages#home'
   get 'signup' => 'users#new'
+  resources :users
   # get 'help' => 'static_pages#help'
 
   # The priority is based upon order of creation: first created -> highest priority.
